ee7ed7ca9e7c2107a16b3a28feb5cddacd77e757
Brunoebaraujo <brunoebaraujo@gmail.com>
2026-05-28 09:25:00 -0300

    Render packing object as transformable group

--- src/components/PackingObject.tsx
+++ src/components/PackingObject.tsx
@@ -1,7 +1,7 @@
 import { Edges } from '@react-three/drei';
 import type { ThreeEvent } from '@react-three/fiber';
-import { forwardRef } from 'react';
-import type { Mesh } from 'three';
+import { forwardRef, useLayoutEffect, useRef } from 'react';
+import type { Group } from 'three';
 import type { PackingObject as PackingObjectType } from '../types';
 import { mmToThreeUnits, packingPositionToThree, rotationDegreesToRadians } from '../utils/unitConversion';
 
@@ -17,12 +17,45 @@ const BOTTOM_COLOR = '#e06a2d';
 const EDGE_SELECTED_COLOR = '#f5c542';
 const EDGE_DEFAULT_COLOR = '#20262d';
 
-export const PackingObject = forwardRef<Mesh, PackingObjectProps>(function PackingObject(
+export const PackingObject = forwardRef<Group, PackingObjectProps>(function PackingObject(
   { object, isSelected, onSelect },
-  ref,
+  forwardedRef,
 ) {
+  const groupRef = useRef<Group | null>(null);
   const { width, depth, height } = object.dimensions;
   const opacity = isSelected ? 0.92 : 0.78;
+
+  const setGroupRef = (group: Group | null) => {
+    groupRef.current = group;
+
+    if (typeof forwardedRef === 'function') {
+      forwardedRef(group);
+    } else if (forwardedRef) {
+      forwardedRef.current = group;
+    }
+  };
+
+  useLayoutEffect(() => {
+    const group = groupRef.current;
+
+    if (!group) {
+      return;
+    }
+
+    const [x, y, z] = packingPositionToThree(object.position);
+    const [rotationX, rotationY, rotationZ] = rotationDegreesToRadians(object.rotation);
+
+    group.position.set(x, y, z);
+    group.rotation.set(rotationX, rotationY, rotationZ, 'XYZ');
+    group.updateMatrixWorld(true);
+  }, [
+    object.position.x,
+    object.position.y,
+    object.position.z,
+    object.rotation.x,
+    object.rotation.y,
+    object.rotation.z,
+  ]);
 
   const handleClick = (event: ThreeEvent<MouseEvent>) => {
     event.stopPropagation();
@@ -37,40 +70,35 @@ export const PackingObject = forwardRef<Mesh, PackingObjectProps>(function Packi
   };
 
   return (
-    <mesh
-      ref={ref}
-      position={packingPositionToThree(object.position)}
-      rotation={rotationDegreesToRadians(object.rotation)}
-      onClick={handleClick}
-      castShadow
-      receiveShadow
-    >
-      {object.type === 'cylinder' ? (
-        <>
-          <cylinderGeometry
-            args={[
-              mmToThreeUnits(width / 2),
-              mmToThreeUnits(width / 2),
-              mmToThreeUnits(height),
-              40,
-            ]}
-          />
-          <meshStandardMaterial attach="material-0" color={BODY_COLOR} {...materialProps} />
-          <meshStandardMaterial attach="material-1" color={TOP_COLOR} {...materialProps} />
-          <meshStandardMaterial attach="material-2" color={BOTTOM_COLOR} {...materialProps} />
-        </>
-      ) : (
-        <>
-          <boxGeometry args={[mmToThreeUnits(width), mmToThreeUnits(height), mmToThreeUnits(depth)]} />
-          <meshStandardMaterial attach="material-0" color={BODY_COLOR} {...materialProps} />
-          <meshStandardMaterial attach="material-1" color={BODY_COLOR} {...materialProps} />
-          <meshStandardMaterial attach="material-2" color={TOP_COLOR} {...materialProps} />
-          <meshStandardMaterial attach="material-3" color={BOTTOM_COLOR} {...materialProps} />
-          <meshStandardMaterial attach="material-4" color={BODY_COLOR} {...materialProps} />
-          <meshStandardMaterial attach="material-5" color={BODY_COLOR} {...materialProps} />
-        </>
-      )}
-      <Edges color={isSelected ? EDGE_SELECTED_COLOR : EDGE_DEFAULT_COLOR} />
-    </mesh>
+    <group ref={setGroupRef} onClick={handleClick}>
+      <mesh castShadow receiveShadow>
+        {object.type === 'cylinder' ? (
+          <>
+            <cylinderGeometry
+              args={[
+                mmToThreeUnits(width / 2),
+                mmToThreeUnits(width / 2),
+                mmToThreeUnits(height),
+                40,
+              ]}
+            />
+            <meshStandardMaterial attach="material-0" color={BODY_COLOR} {...materialProps} />
+            <meshStandardMaterial attach="material-1" color={TOP_COLOR} {...materialProps} />
+            <meshStandardMaterial attach="material-2" color={BOTTOM_COLOR} {...materialProps} />
+          </>
+        ) : (
+          <>
+            <boxGeometry args={[mmToThreeUnits(width), mmToThreeUnits(height), mmToThreeUnits(depth)]} />
+            <meshStandardMaterial attach="material-0" color={BODY_COLOR} {...materialProps} />
+            <meshStandardMaterial attach="material-1" color={BODY_COLOR} {...materialProps} />
+            <meshStandardMaterial attach="material-2" color={TOP_COLOR} {...materialProps} />
+            <meshStandardMaterial attach="material-3" color={BOTTOM_COLOR} {...materialProps} />
+            <meshStandardMaterial attach="material-4" color={BODY_COLOR} {...materialProps} />
+            <meshStandardMaterial attach="material-5" color={BODY_COLOR} {...materialProps} />
+          </>
+        )}
+        <Edges color={isSelected ? EDGE_SELECTED_COLOR : EDGE_DEFAULT_COLOR} />
+      </mesh>
+    </group>
   );
 });
